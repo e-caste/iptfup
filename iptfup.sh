1fc2040f2aa9cc932ea643ef15bd9e47a7b0649d
alecello <alessandro.celoria@hotmail.com>
2020-08-27 14:27:09 +0200

    Fixes, added timer environment variable

--- iptfup.sh
+++ iptfup.sh
@@ -17,18 +17,33 @@ function sigint_handler {
 
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
 	echo "This script is intended to prevent fuck-ups when setting a new iptables rule."
-	echo "Simply run 'sudo $0 iptables <iptables options>' to automatically restore the previous rules in case of a fuck-up, like preventing all incoming TCP traffic."
+	echo "Simply run either;"
+	echo "    - 'sudo $0 iptables <iptables options>'"
+	echo "    - 'sudo $0 iptables-restore <options> < /path/to/iptables.rules'"
+	echo "to automatically restore the previous rules in case of a fuck-up, like preventing all incoming TCP traffic."
+	echo ""
 	echo "For a more comfortable experience, you can either:"
-	echo "- add an alias for iptables in your shell rc file like 'alias iptables=\"sudo $0 iptables\"'"
-	echo "- add this repository's directory to your PATH"
-	echo "- copy this script to a directory in your PATH, like /usr/local/bin"
+	echo "    - add an alias for iptables in your shell rc file like 'alias iptables=\"sudo $0 iptables\"' and 'alias iptables-restore=\"sudo $0 iptables-restore\"'"
+	echo "    - add this repository's directory to your PATH"
+	echo "    - copy this script to a directory in your PATH, like /usr/local/bin"
 	exit 0
 fi
 
-# if iptables -L run without any prevention
-if [ "$2" = "-L" ]; then
-	"$@"
-	exit 0
+if [ "$1" == "iptables" ]; then
+	if [ $# -eq 1 ]; then echo "iptables requires at least one argument. Aborting..."; exit -1; fi
+
+	command -v iptables &> /dev/null
+	if [ $? -ne 0 ]; then echo "This script is called iptables-fuck-up-prevention, it is useless without iptables. Aborting..."; exit -1; fi
+fi
+
+if [ "$1" == "iptables-restore" ]; then
+	command -v iptables-restore &> /dev/null
+	if [ $? -ne 0 ]; then echo "This script was unable to locate the iptables-restore binary. Aborting..."; exit -1; fi
+fi
+
+if [ "$1" != "iptables" -a "$1" != "iptables-restore" ]; then
+	echo "I see you're a funny guy. This script only works with iptables and iptables-restore though. Aborting..."
+	exit -1
 fi
 
 if [ "$EUID" -ne 0 ]; then
@@ -38,20 +53,10 @@ if [ "$EUID" -ne 0 ]; then
 	exit -1
 fi
 
-command -v iptables &> /dev/null
-if [ $? -ne 0 ]; then
-	echo "This script is called iptables-fuck-up-prevention, it is useless without iptables. Aborting..."
-	exit -1
-fi
-
-if [ "$1" != "iptables" -a "$1" != "iptables-restore" ]; then
-	echo "I see you're a funny guy. This script only works with iptables and iptables-restore though. Aborting..."
-	exit -1
-fi
-
-if [ "$1" == "iptables" -a $# -eq 1 ]; then
-	echo "iptables requires a rule as argument. Aborting..."
-	exit -1
+# if iptables -L run without any prevention
+if [ "$2" = "-L" ]; then
+	"$@"
+	exit 0
 fi
 
 trap sigint_handler INT
@@ -61,33 +66,39 @@ if [ -f $tmp_file ]; then
 	echo "$tmp_file already exists in this directory. Please rename it, move it or delete it to proceed."
 	exit -1
 fi
-iptables-save > $tmp_file
 
-	echo "Running $@ as root."
-	echo "If something goes wrong, this script will restore the previous iptables rules in 10 seconds."
-	echo "" 
-	if [ "$1" == "iptables" ]; then
-		# run iptables command unfolding all arguments
-		"$@"
-	elif [ "$1" == "iptables-restore" ]; then
-		# run iptables-restore command passing stdin
-		"$@" <&0
-	fi
+# if the IPTFUP_TIMER environment variable is defined and is a valid number, set the timer to that value
+if [ ! -z "${IPTFUP_TIMER##*[!0-9]*}" ]; then
+	timer=$IPTFUP_TIMER
+else
+	# iptfup default
+	timer=10
+fi
+
+iptables-save > $tmp_file
+	
+# run command unfolding all arguments (and passing stdin if the command is iptables-restore)
+echo "Running $@ as root."
+echo "If something goes wrong, this script will restore the previous iptables rules in $timer seconds."
+echo ""
+if [ "$1" == "iptables" ]; then
+	"$@"
+elif [ "$1" == "iptables-restore" ]; then
+	"$@" <&0
+fi
 
 # force the read to happen on /dev/tty since if we're handling iptables-restore we have redirected stdin
 echo "Are these firewall rules working? y/N"
-read -t 10 </dev/tty
+read -t $timer ans </dev/tty
 
 echo ""
 
-# if $ans is no or there is no ans since the connection has been closed
-if [ -z $ans ]; then
-	restore_tables
-elif [ $ans = "n" -o $ans = "N" ]; then
-	restore_tables
-# if $ans confirms the rules
-elif [ $ans = "y" -o $ans = "Y" ]; then
+# if $ans is different from case-insensitive y, then restore the rules
+# this can happen if the user presses n or any other key or if the connection got closed by a bad rule
+if [ "$ans" = "y" -o "$ans" = "Y" ]; then
 	echo "Rules applied."
+else
+	restore_tables
 fi
 
 rm $tmp_file
